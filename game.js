3c5b648a5f9aff3c80d654f766025435327517da
Edward Tran <giaphutran012@gmail.com>
2025-06-08 12:26:44 -0700

    added sounds to buttons

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
 let gamePattern=[];
 let buttonColours=["red","green","blue","yellow"];
 
+
 function nextSequence(){
 let ranndomNumber=getRandomInt(4);
 let randomChosenColour=buttonColours[ranndomNumber];
 gamePattern.push(randomChosenColour);
 $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+playAudio(randomChosenColour);
 }
 
 function getRandomInt(max){
     return Math.floor(Math.random()*max);
 }
-nextSequence();
+function playAudio(colorToPlay){
+let audio=new Audio("./sounds/"+colorToPlay+".mp3");
+audio.play();
+}
+$("body").click(function(){nextSequence()});
+
