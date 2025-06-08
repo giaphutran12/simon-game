b671b3b7e2b678ce8efa2c4fd91b0034492fb908
Edward Tran <giaphutran012@gmail.com>
2025-06-07 22:59:14 -0700

    made a random button flash

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-$("h1").html("yo")
+let gamePattern=[];
+let buttonColours=["red","green","blue","yellow"];
+
+function nextSequence(){
+let ranndomNumber=getRandomInt(4);
+let randomChosenColour=buttonColours[ranndomNumber];
+gamePattern.push(randomChosenColour);
+$("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+nextSequence();
